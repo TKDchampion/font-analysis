7005c5fb95ebf94ef77d4f29b6a485de09c6c2fc
Giant <giant@Giantde-MacBook-Pro.local>
2020-06-28 11:33:23 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('src/app/pages/pages.module').then(p => p.PagesModule),
+    // canActivate: [AuthService]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
